f52f538bdfb534d137c621090835c993b5f70562
MikeDabb <mdabrowski1@gmail.com>
2019-10-29 00:15:00 +0100

    Changing light over time + first powerup + added halos for lights

---
+++ Prototype #no/Assets/Scripts/PowerUpController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PowerUpController : MonoBehaviour
+{
+    private Transform transform;
+    private readonly float rotationSpeed = 150f;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();    
+    }
+
+    void Update()
+    {
+        RotateBox();
+
+    }
+
+    private void RotateBox()
+    {
+        transform.Rotate(0, Time.deltaTime * rotationSpeed, 0);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerOne")
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
---
+++ Prototype #no/Assets/Scripts/RotateLightOverTime.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class RotateLightOverTime : MonoBehaviour
+{
+    private Transform transform;
+    public float rotationSpeed;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, Time.deltaTime * rotationSpeed);
+        if (transform.rotation.eulerAngles.z > 90)
+        {
+            rotationSpeed = 3f;
+        }
+
+        else if (transform.rotation.eulerAngles.z > 240)
+        {
+            rotationSpeed = 2f;
+        }
+    }
+}
