3ae505ec098a5424ef4d3661711ceb7af1491514
MikeDabb <mdabrowski1@gmail.com>
2019-12-04 23:33:37 +0100

    first playable version - missing colliders and with bugs

--- Prototype #no/Assets/Scripts/Game Manager/GameManager.cs
+++ Prototype #no/Assets/Scripts/Game Manager/GameManager.cs
@@ -134,13 +134,13 @@ public class GameManager : MonoBehaviourPunCallbacks
             {
                 Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
                 // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
-                PhotonNetwork.Instantiate(playerOnePrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                PhotonNetwork.Instantiate(playerOnePrefab.name, new Vector3(-12f, 5f, 0f), Quaternion.identity, 0);
             }
             else
             {
                 Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
                 // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
-                PhotonNetwork.Instantiate(playerTwoPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                PhotonNetwork.Instantiate(playerTwoPrefab.name, new Vector3(12f, 5f, 0f), Quaternion.identity, 0);
             }
         }
         else
